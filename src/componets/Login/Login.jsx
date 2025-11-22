916fa3b0268e9cf244c5a5ca8b0d8aa8d914857f
mehadi <mehadishisir@gmail.com>
2025-11-22 18:23:06 +0600

    add log in with google

--- src/componets/Login/Login.jsx
+++ src/componets/Login/Login.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../../Firebase/firebase.init";
 
 const Login = () => {
+  const handleGoogleLogin = () => {
+    console.log("clicked");
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div>
       <h2>Please Log in</h2>
+      <button onClick={handleGoogleLogin}>sign in with google</button>
     </div>
   );
 };
