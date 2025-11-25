e37250558560e53f772f3beef152edf7a27261d2
mehadi <mehadishisir@gmail.com>
2025-11-25 16:28:34 +0600

    add github login

--- src/componets/Login/Login.jsx
+++ src/componets/Login/Login.jsx
@@ -1,4 +1,9 @@
-import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import { auth } from "../../Firebase/firebase.init";
 import { useState } from "react";
 
@@ -7,6 +12,18 @@ const Login = () => {
   const handleGoogleLogin = () => {
     console.log("clicked");
     const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result.user);
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const handleGithubLogin = () => {
+    console.log("github clicked");
+    const provider = new GithubAuthProvider();
     signInWithPopup(auth, provider)
       .then((result) => {
         console.log(result.user);
@@ -33,12 +50,16 @@ const Login = () => {
       {user ? (
         <button onClick={handleSignOut}>sign out</button>
       ) : (
-        <button onClick={handleGoogleLogin}>sign in with google</button>
+        <>
+          <button onClick={handleGoogleLogin}>Google Login</button>
+          <button onClick={handleGithubLogin}>GitHub Login</button>
+        </>
       )}
       {user && (
         <div>
           <h4>{user.displayName}</h4>
           <p>{user.email}</p>
+          <img src={user.photoURL} alt=""></img>
         </div>
       )}
     </div>
